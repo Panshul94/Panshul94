22d318de961a7681cdfbb8880af6ec236274cbda
Panshul94 <68825755+Panshul94@users.noreply.github.com>
2021-06-12 16:20:41 +0530

    Add files via upload
    FIles having application code.

---
+++ src/com/calculator/ColloFrame.java
@@ -0,0 +1,26 @@
+package com.calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ColloFrame {
+
+    List<Integer> integerList = new ArrayList<Integer>();
+
+    //method 1
+    public void addElement(int i){
+
+        //Integer element = new Integer(i);
+
+        integerList.add(i);
+
+    }
+
+    //method 2
+
+    //method 3
+
+    //method 4
+
+
+}
---
+++ src/com/calculator/MyCalculatorImplementation.java
@@ -0,0 +1,28 @@
+package com.calculator;
+
+public class MyCalculatorImplementation {
+
+
+
+
+    public int add(int x, int y){
+
+        return x+y;
+    }
+
+    public int subtract(int x, int y){
+
+        return x-y;
+    }
+
+    public int multiply(int x, int y){
+
+        return x*y;
+    }
+
+    public int divide(int x, int y){
+
+        return x/y;
+    }
+
+}
